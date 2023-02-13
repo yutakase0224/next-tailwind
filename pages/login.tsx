3a9f944f402c822c0c3e7b9b3ea2c52d351be7b5
sha264 <marumarusasa@gmail.com>
2023-02-13 16:28:08 +0900

    A

--- pages/login.tsx
+++ pages/login.tsx
@@ -24,7 +24,7 @@ const Login = () => {
             placeholder="Enter your email"
           />
           <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-            <a href="https://npofos.or.jp/event">
+            <a href="https://npofos.or.jp">
             <button
               type="submit"
               className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
