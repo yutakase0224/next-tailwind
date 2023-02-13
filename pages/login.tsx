4f879dcad55cbcece45d4c7422ed7c3d6a925f10
sha264 <marumarusasa@gmail.com>
2023-02-13 15:45:22 +0900

    title red

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,8 +3,8 @@ import { join } from "path";
 const Login = () => {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-full py-12 px-6 lg:py-16 lg:px-8">
-        <h2 className="inline text-3xl font-bold tracking-tight text-gray-900 sm:block sm:text-4xl">
+      <div className="mx-auto max-w-7xl py-12 px-6 lg:py-16 lg:px-8">
+        <h2 className="inline text-3xl font-bold tracking-tight text-red-900 sm:block sm:text-4xl">
           Login Form
         </h2>
         <p className="inline text-3xl font-bold tracking-tight text-indigo-600 sm:block sm:text-4xl">
